8bddeaec7c128270e939aae3470cf3bd388c9b05
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-09-14 03:43:52 +0530

    middleware

---
+++ middleware.js
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+  console.log("MiddleWare Executed")
+  const authToken = request.cookies.get("JWT")?.value;
+  if(request.nextUrl.pathname==="/api/users/signin/route") return;
+  console.log(authToken)
+
+  const loggedInUserNotAccessPath = request.nextUrl.pathname === "/components/LoginPage/login" || request.nextUrl.pathname === "/components/signup/Signup" || request.nextUrl.pathname === "/api/users/signin"
+
+  if(loggedInUserNotAccessPath){
+    if(authToken){
+        return NextResponse.redirect(new URL("/components/Dashboard/dashboard",request.url))
+    }
+  } else{
+    if(!authToken){
+        return NextResponse.redirect(new URL("/components/LoginPage/login",request.url))
+    }
+  }
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/components/LoginPage/login','/components/signup/Signup','/components/Dashboard/dashboard','/api/users/:path*'],
+}
