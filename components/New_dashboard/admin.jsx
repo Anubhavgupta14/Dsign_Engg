4f9aa82bfd7b8761a1ea9ec40dc0e6ecbedec746
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-10-11 00:07:21 +0530

    admin secure

--- components/New_dashboard/admin.jsx
+++ components/New_dashboard/admin.jsx
@@ -1,15 +1,17 @@
 // import ProfileLayout from "../../components/user_data/User_security";
 import { useState, useEffect } from "react";
-// import { fetchCurrentUser } from "libs/fetchUser";
-import { useRouter } from "next/router";
 import { toast } from "react-toastify";
 import Navbar from "../navbar/Navbar";
 import Footer from "../Footer/footer";
+import { fetchCurrentUser } from '../../libs/fetchUser';
 import Box from "@mui/material/Box";
 import { DataGrid } from '@mui/x-data-grid';
+import { useRouter } from 'next/navigation';
+import Link from "next/link";
 import Expand from './expand'
 
 const general = () => {
+  const router = useRouter();
   const [data, setData] = useState([]);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
@@ -19,6 +21,21 @@ const general = () => {
   setOpen(true);
   setExpandEmail(email);
   };
+
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    state: '',
+    city: '',
+    pincode: '',
+    dob: '',
+    gender: '',
+    addressLine1: '',
+    addressLine2: '',
+    basicProfile: '',
+    role: 'user', // Default role
+  });
 
 
   const columns = [
@@ -76,7 +93,33 @@ const general = () => {
     aod_ticket: user.aod_ticket,
     
   }));
-  
+
+ 
+
+  const getUserData = async () => {
+    const token = localStorage.getItem('JWT');
+    if(token==null) return;
+    try {
+      const data = await fetchCurrentUser(token);
+      console.log(data, 'data fetched');
+      const { error } = data;
+      console.log(error, 'error getting user data');
+      if (error) {
+        toast.error(error);
+        return;
+      }
+      let user = data;
+      const convertedUser = {
+        ...user,
+        dob: user.dob ? user.dob.split('T')[0] : '',
+      };
+      setUserData(convertedUser);
+    } catch (error) {
+      toast.error(error.message + 'op' || 'Some error occurred while fetching data');
+    }
+    
+  };
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -86,35 +129,75 @@ const general = () => {
       setData(data);
     };
 
-    fetchData();
+    const check = () => {
+      if (userData.role !== 'admin') {
+        alert("hello")
+        console.log("wapas")
+        router.push('/dashboard')
+      } else {
+        fetchData();
+      }
+    };
+
+    // useEffect specifically for userData.role changes
+    const handleRoleChange = () => {
+      if (userData.role === 'admin') {
+        check();
+      }
+      else{
+        // alert('ok')
+        // router.push('/dashboard')
+      }
+    };
+
+    // Watch for changes in userData.role
+    handleRoleChange();
+
+  }, [userData.role]); // Add userData.role as a dependency here
+
+  useEffect(() => {
+    const getUserDataAndCheck = async () => {
+      try {
+        await getUserData();
+      } catch (error) {
+        console.log("error")
+      }
+    };
+
+    getUserDataAndCheck();
   }, []);
 
   return (
     <>
       <Navbar />
-      <div className="main_admin">
-        {/* <Expand open={open} handleClose={handleClose} email={expandEmail}/> */}
-        <div className="container_admin">
-        <p className="profile-t">Admin Portel</p>
-        <div className="my-2"></div>
-          <Box sx={{ height: 400, width: '100%' }}>
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              initialState={{
-                pagination: {
-                  paginationModel: {
-                    pageSize: 5,
+      {userData.role === 'admin' ? (
+        <div className="main_admin">
+          <div className="container_admin">
+            <p className="profile-t">Admin Portel</p>
+            <div className="my-2"></div>
+            <Box sx={{ height: 400, width: '100%' }}>
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{
+                  pagination: {
+                    paginationModel: {
+                      pageSize: 5,
+                    },
                   },
-                },
-              }}
-              pageSizeOptions={[5]}
-              checkboxSelection
-              disableRowSelectionOnClick
-            />
-          </Box>
+                }}
+                pageSizeOptions={[5]}
+                checkboxSelection
+                disableRowSelectionOnClick
+              />
+            </Box>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="notfound">
+        <h2 className="notfound_h notfound_p">404 Not Found</h2>
+        </div>
+      )}
       <Footer />
     </>
   );
