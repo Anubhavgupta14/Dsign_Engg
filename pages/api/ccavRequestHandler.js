b3896ea7ab5ad5eedc7f583e7d89a331bfe4da23
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-11-05 21:05:16 +0530

    fix

--- pages/api/ccavRequestHandler.js
+++ pages/api/ccavRequestHandler.js
@@ -7,15 +7,15 @@ export default async function handler(req, res) {
   
   const paymentDetails = req.body; // Assuming the payment details object is passed in the request body
   console.log(paymentDetails, " payment details received in server");
-  const workingKey = "BF175DDE59AF4B0988BF025F94F3DCB2" // Replace with your actual working key
-  const accessCode = "AVVL05KH14CC75LVCC" // Replace with your actual access code
+  const workingKey = process.env.CCAVENUEWORKINGID; // Replace with your actual working key
+  const accessCode = process.env.CCAVENUEACCESSCODE; // Replace with your actual access code
 
   const md5 = crypto.createHash('md5').update(workingKey).digest();
   const keyBase64 = Buffer.from(md5).toString('base64');
 
   const ivBase64 = Buffer.from([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]).toString('base64');
 
-  let str = `merchant_id=2751408`;
+  let str = process.env.MERCHANTID;
   for (const key in paymentDetails) {
     str+=`&${key}=${paymentDetails[key]}`;
   }
