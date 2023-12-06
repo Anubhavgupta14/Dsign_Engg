9d13fe100daf74545f7f523e71b126c74ead7c8f
Anubhav Gupta <97956667+Anubhavgupta14@users.noreply.github.com>
2023-12-07 04:00:27 +0530

    Update dashboard_history.js

--- pages/dashboard_history.js
+++ pages/dashboard_history.js
@@ -1,16 +1,1 @@
-import React from 'react'
-import History from '../components/Dashboard_history/history'
 
-const dashboard_history = ({token}) => {
-  return (
-    <>
-      <History authtoken={token}/>
-    </>
-  )
-}
-
-export default dashboard_history
-
-export function getServerSideProps({ req, res }) {
-  return { props: { token: req.cookies.JWT || "" } };
-}
